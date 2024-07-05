76971d9e7bcf10282e02819790e22859452c77f6
Le Xuan Vu <lexuanvu161@gmail.com>
2024-07-05 20:41:39 +0700

    Finish Earth project

---
+++ Getting Started/script.js
@@ -0,0 +1,48 @@
+import * as THREE from "three"
+import { OrbitControls } from "jsm/controls/OrbitControls.js"
+
+const w = window.innerWidth
+const h = window.innerHeight
+const renderer = new THREE.WebGLRenderer({ antialias: true })
+renderer.setSize(w, h)
+document.body.appendChild(renderer.domElement)
+
+const fov = 75;
+const aspect = w / h
+const near = 0.1
+const far = 10
+const camera = new THREE.PerspectiveCamera(fov, aspect, near, far)
+camera.position.z = 2
+
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.enableDamping = true
+controls.dampingFactor = 0.03
+const scene = new THREE.Scene()
+
+const geo = new THREE.IcosahedronGeometry(1.0, 3)
+const mat = new THREE.MeshStandardMaterial({
+    color: 0xffffff,
+    flatShading: true
+})
+const mesh = new THREE.Mesh(geo, mat)
+scene.add(mesh)
+
+const wireMat = new THREE.MeshBasicMaterial({
+    color: 0xffffff,
+    wireframe: true
+})
+const wireMesh = new THREE.Mesh(geo, wireMat)
+wireMesh.scale.setScalar(1.005)
+mesh.add(wireMesh)
+
+const hemiLight = new THREE.HemisphereLight(0x0099ff, 0xaa5500)
+scene.add(hemiLight)
+
+function animate(t = 0) {
+    requestAnimationFrame(animate)
+    // mesh.rotation.y = t * 0.0003
+    renderer.render(scene, camera)
+    controls.update()
+}
+
+animate()
